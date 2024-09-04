ef639abaf673c82494eb429d968588cceaf93914
spacemapache <142768034+spacemapache@users.noreply.github.com>
2024-09-04 14:02:25 -0700

    updated variables for items in javascript

---
+++ src/index.js
@@ -0,0 +1,20 @@
+// change tip amount/ person
+// add amount to bill - button to click on tip % and number of ppl.
+// formula for amount for each person will appear
+// formula for total /person
+
+const bill = document.getElementById("billInput");
+const numPeople = document.getElementById("peopleInput");
+const tipPerson = document.getElementById("amountPerson");
+const totalPerson = document.getElementById("totalPerson");
+
+const fifteenPercent = document.getElementById("15Btn");
+const twentyPercent = document.getElementById("20Btn");
+const customPercent = document.getElementById("customInput");
+
+function tipAmount() {}
+
+function reset() {
+  event.preventDefault();
+  alert("reset");
+}
